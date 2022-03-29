5f0aaa1a9111a3e64a9f582066adba0822e93bbc
yewonJin <jyw966@naver.com>
2022-03-29 21:23:27 +0900

    nav-bar ver1

--- Web/nav-bar/script.js
+++ Web/nav-bar/script.js
@@ -1,1 +1,7 @@
+const $img = document.getElementById('menu_img');
 
+$img.addEventListener("click", () => {
+    document.querySelector('.menu').classList.toggle("open");
+    document.querySelector('.expand_menu').classList.toggle("open"); 
+    document.querySelector('.narrow_menu').classList.toggle("open");
+});
